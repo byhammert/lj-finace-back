e82014900851f2a53cf28e75dad57fb6ded74283
Luiz Jones <luiz.jones@external.stellantis.com>
2023-04-19 09:31:52 -0300

    corrigindo dependencias do gradle

--- infrastructure/src/main/java/com/lj/infrastructure/Main.java
+++ infrastructure/src/main/java/com/lj/infrastructure/Main.java
@@ -1,10 +1,7 @@
 package com.lj.infrastructure;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
@@ -12,10 +9,5 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(Main.class, args);
-    }
-
-    @Bean
-    public KeycloakConfigResolver keycloakConfigResolver(){
-        return new KeycloakSpringBootConfigResolver();
     }
 }
